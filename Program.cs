dbe2a081b29964ad8368d6549213ae1e7761dbf9
Ekaterina <voronova-e@mail.ru>
2023-04-09 19:15:24 +0300

    Сложение четырёх чисел

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿int numberA = 10;
 int numberB = 23;
 int numberC = 76;
-int result = numberA + numberB + numberC;
+int numberD = 87;
+int result = numberA + numberB + numberC + numberD;
 Console.WriteLine(result);
